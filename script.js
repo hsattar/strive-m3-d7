97b5806cb2b7ac4338b550d462a38e4b75b86dfb
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-02 17:47:39 +0000

    added ability to filter resu;ts

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let userData = []
+
 const fetchData = async () => {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users')
         const data = await response.json()
+        userData = data
         displayData(data)
     } catch (err) {
         console.error(err)
@@ -10,8 +13,7 @@ const fetchData = async () => {
 
 fetchData()
 
-const displayData = async data => {
-    console.log(data)
+const displayData = data => {
     const tableBody = document.querySelector('.tableBody')
     tableBody.innerHTML = data.map(user => `
     <tr>
@@ -19,8 +21,22 @@ const displayData = async data => {
         <td>${user.username}</td>
         <td>${user.name}</td>
         <td>${user.email}</td>
-        <td>${user.phone}</td>
-        <td>${user.website}</td>
     </tr>`).join('')
 }
 
+const filterResults = (filterHeading, input) => {
+    const filteredData = userData.filter(user => user[filterHeading.toLowerCase()].toLowerCase().includes(input.toLowerCase()))
+    displayData(filteredData)
+}
+
+const filterCategory = document.querySelector('#filterCategory')
+const filterInput = document.querySelector('#filterInput')
+
+filterCategory.addEventListener('change', () => {
+    filterInput.placeholder = `Filter By ${filterCategory.value}`
+    filterResults(filterCategory.value, filterInput.value)
+})
+
+filterInput.addEventListener('keyup', () => {
+    filterResults(filterCategory.value, filterInput.value)
+})
